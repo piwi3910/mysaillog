87f62abe49139b82ec606f506bd590ad3608290b
Pascal Watteel <Pascal@watteel.com>
2025-02-07 21:10:25 +0400

    Add reanimated and gesture handler configuration

--- frontend/index.ts
+++ frontend/index.ts
@@ -1,8 +1,6 @@
 import { registerRootComponent } from 'expo';
-
+import 'react-native-gesture-handler';
 import App from './App';
 
-// registerRootComponent calls AppRegistry.registerComponent('main', () => App);
-// It also ensures that whether you load the app in Expo Go or in a native build,
-// the environment is set up appropriately
+// Initialize gesture handler before registering the root component
 registerRootComponent(App);
